34fd13a83b9d410084e686ab332253fe8c1c0d25
Julius <juliusarthur2005@gmail.com>
2025-05-09 22:09:08 +0700

    fix: recipe limit for single recipe

--- src/bfs.go
+++ src/bfs.go
@@ -45,6 +45,65 @@ import (
 // 	}
 // }
 
+func bfs_single(root *ElementNode, elements map[string]*ElementNode, recipes []RecipeNode) {
+	q := []*ElementNode{root}
+	visited := make(map[string]bool)
+	visited[root.Name] = true
+	root.IsVisited = true
+
+	for len(q) > 0 {
+		current := q[0]
+		// fmt.Println(current.Name)
+
+		for _, recipe := range recipes {
+			base1, ok1 := elements[recipe.Ingredient1.Name]
+			base2, ok2 := elements[recipe.Ingredient2.Name]
+
+			if !ok1 || !ok2 {
+				continue
+			}
+
+			if recipe.Result != current.Name {
+				continue
+			}
+
+			if base1.Tier > current.Tier || base2.Tier > current.Tier {
+				continue
+			}
+
+			if ok1 && ok2 {
+				if len(current.Children) < 1 {
+
+					current.Children = append(current.Children, &RecipeNode{
+						Result:      current.Name,
+						Ingredient1: base1,
+						Ingredient2: base2,
+					})
+					//BFS
+
+					if !visited[base1.Name] {
+						// fmt.Println("Enque: ", base1.Name)
+						//Enqueue
+						q = append(q, base1)
+						visited[base1.Name] = true
+						base1.IsVisited = true
+					}
+
+					if !visited[base2.Name] {
+						//Enqueue
+						q = append(q, base2)
+						visited[base2.Name] = true
+						base2.IsVisited = true
+					}
+				}
+			}
+		}
+		//Dequeue
+		q = q[1:]
+
+	}
+}
+
 func bfs(root *ElementNode, elements map[string]*ElementNode, recipes []RecipeNode) {
 	q := []*ElementNode{root}
 	visited := make(map[string]bool)
